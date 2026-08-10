852e201118ce5d7262bd02f73443f4e15644c899
Alexandre Nazato <dreamcraftfestas@gmail.com>
2026-08-10 14:29:17 -0300

    Imagens

--- scriptcompleta.js
+++ scriptcompleta.js
@@ -111,7 +111,7 @@ const bancoDadosTemas = [
     { nome: "Rapunzel Castelo",                   pasta: "rapunzelcastelo", categorias: "all princesas contodefadas filme"   , totalImgs: 1 },
     { nome: "Rei Leão",                           pasta: "reileao", categorias: "all desenho filme"   , totalImgs: 1 },
     { nome: "Roblox",                             pasta: "roblox", categorias: "all jogos"   , totalImgs: 1 },
-    { nome: "Rosas Azuis",                        pasta: "rosasazuis", categorias: "all jogos"   , totalImgs: 0 },S
+    { nome: "Rosas Azuis",                        pasta: "rosasazuis", categorias: "all jogos"   , totalImgs: 0 },
     { nome: "Sereia Splash",                      pasta: "sereiasplash", categorias: "all fundodomar desenhos"   , totalImgs: 0 },
     { nome: "Santos",                             pasta: "santos", categorias: "all futebol"   , totalImgs: 1 },
     { nome: "Shimmer Wall Holográfico/Frutacor",  pasta: "shimmerwallholograficofrutacor", categorias: "all diversos"   , totalImgs: 0 },
